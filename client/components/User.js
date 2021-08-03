5b34f4ee8839095d0108cebf2accbafebad33140
steph durino <steph.durino@gmail.com>
2021-08-03 14:55:04 -0400

    trying something

--- client/components/User.js
+++ client/components/User.js
@@ -6,56 +6,68 @@ import { fetchSingleUser } from '../store/user';
 //import LoadingScreen from './LoadingScreen';
 
 class User extends React.Component {
-  constructor(){
-    super();
+	constructor() {
+		super();
 
-    this.state = {
-      userId: '',
-      username: '',
-      cart: []
-    };
-  }
+		this.state = {
+			username: '',
+			password: '',
+			cc: null,
+			shippingAddress: null,
+			billingAddress: null,
+			cart: [],
+			prevOrders: [],
+			favProducts: [],
+			isAdmin: false,
+		};
+	}
 
-  componentDidMount() {
-    this.props.getSingleUser(this.state.userId, this.state.username);
-  }
+	componentDidMount() {
+		this.props.getSingleUser(this.state.userId, this.state.username);
+	}
 
-  componentDidUpdate(prevProps) {
-    //we might need this?
+	componentDidUpdate(prevProps) {
+		//we might need this?
+		// if (!Array.isArray(this.props.robot.projects) || !Array.isArray(prevProps.robot.projects) || (prevProps.robot.projects.length !== this.props.robot.projects.length)) {
+		//   this.props.getSingleRobot(this.props.match.params.robotId);
+		// }
+	}
 
-    // if (!Array.isArray(this.props.robot.projects) || !Array.isArray(prevProps.robot.projects) || (prevProps.robot.projects.length !== this.props.robot.projects.length)) {
-    //   this.props.getSingleRobot(this.props.match.params.robotId);
-    // }
-  }
+	render() {
+		//if (this.props.robot[0] === 'error') { return <ErrorHandler /> }
+		//else if (!this.props.robot.id) { return <LoadingScreen />}
 
-  render(){
-    //if (this.props.robot[0] === 'error') { return <ErrorHandler /> }
-    //else if (!this.props.robot.id) { return <LoadingScreen />}
+		return (
+			//
+			//insert cart component somewhere that makes sense
 
-
-    return (
-      //
-      //insert cart component somewhere that makes sense
-
-      <div id="single-user">
-        <div key={thisUser.id} className="single-user-entry">
-          <h2 className="user-name">Welcome, {thisUser.name}! What a purrfect day!</h2>
-          <div><Link to={`/user/edit/${thisUser.id}`}><button type="button" className="edit-button" name={thisUser.id} >Edit Profile</button></Link></div>
-        </div>
-      </div>
-    )
-  }
+			<div id='single-user'>
+				<div key={thisUser.id} className='single-user-entry'>
+					<h2 className='user-name'>
+						Welcome, {thisUser.name}! What a purrfect day!
+					</h2>
+					<div>
+						<Link to={`/user/edit/${thisUser.id}`}>
+							<button type='button' className='edit-button' name={thisUser.id}>
+								Edit Profile
+							</button>
+						</Link>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 const mapState = (state) => {
-  return { user: state.user }
-}
+	return { user: state.user };
+};
 const mapDispatch = (dispatch) => {
-  return {
-    getUser: (userId, username) => dispatch(fetchUser(userId, username))
-    //,
-    //edit thunk?
-  }
-}
+	return {
+		getUser: (userId, username) => dispatch(fetchUser(userId, username)),
+		//,
+		//edit thunk?
+	};
+};
 
 export default connect(mapState, mapDispatch)(User);
