4a141b6cf7947f8ecead3107bfb2f8dd6e6bed0a
steph durino <steph.durino@gmail.com>
2021-08-03 16:28:59 -0400

    maybe did all of these right?- sd

---
+++ client/components/AllProducts.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchProducts } from '../store/products';
+
+class AllProducts extends React.Component {
+	componentDidMount() {
+		this.props.getProducts();
+	}
+
+	render() {
+		const { products } = this.props;
+		return (
+			<div>
+				<h1>SHOP MEOW!</h1>
+				<div>
+					{products.length > 0 ? (
+						<div>
+							{products.map((product) => {
+								return (
+									<div key={product.id}>
+										<Link to={`/products/${product.id}`}>
+											<img className='product-image' src={product.imageUrl} />
+										</Link>
+										<p>{product.name}</p>
+										<p>
+											{/* <button
+												className='remove'
+												onClick={() => this.props.deleteRobot(robot.id)}>
+												delete
+											</button> */}
+										</p>
+									</div>
+								);
+							})}
+						</div>
+					) : (
+						<div>Cats destroyed everything, run for your life!</div>
+					)}
+				</div>
+			</div>
+		);
+	}
+}
+
+const mapState = (state) => ({
+	products: state.products,
+});
+
+const mapDispatch = (dispatch) => ({
+	getProducts: () => dispatch(fetchProducts()),
+});
+
+export default connect(mapState, mapDispatch)(AllProducts);
--- client/components/SingleUser.js
+++ client/components/SingleUser.js
@@ -2,38 +2,41 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { fetchSingleUser } from '../store/user';
+
 //import ErrorHandler from './ErrorHandler';
 //import LoadingScreen from './LoadingScreen';
 
 class User extends React.Component {
-	constructor() {
-		super();
-
-		this.state = {
-			username: '',
-			password: '',
-			cc: null,
-			shippingAddress: null,
-			billingAddress: null,
-			cart: [],
-			prevOrders: [],
-			favProducts: [],
-			isAdmin: false,
-		};
-	}
+	// constructor() { -- sd
+	// 	super();
+	//may not need for store??? refer to store???
+	// this.state = {
+	// 	username: '',
+	// 	password: '',
+	// 	cc: null,
+	// 	shippingAddress: null,
+	// 	billingAddress: null,
+	// 	cart: [],
+	// 	prevOrders: [],
+	// 	favProducts: [],
+	// 	isAdmin: false,
+	// };
+	//}
 
 	componentDidMount() {
-		this.props.getSingleUser(this.state.userId, this.state.username);
+		this.props.getSingleUser(this.props.user.userId, this.props.user.username);
 	}
 
-	componentDidUpdate(prevProps) {
-		//we might need this?
-		// if (!Array.isArray(this.props.robot.projects) || !Array.isArray(prevProps.robot.projects) || (prevProps.robot.projects.length !== this.props.robot.projects.length)) {
-		//   this.props.getSingleRobot(this.props.match.params.robotId);
-		// }
-	}
+	//idk if we need this yet. - sd
+	// componentDidUpdate(prevProps) {
+	// 	//we might need this?
+	// 	// if (!Array.isArray(this.props.robot.projects) || !Array.isArray(prevProps.robot.projects) || (prevProps.robot.projects.length !== this.props.robot.projects.length)) {
+	// 	//   this.props.getSingleRobot(this.props.match.params.robotId);
+	// 	// }
+	// }
 
 	render() {
+		const { user } = this.props;
 		//if (this.props.robot[0] === 'error') { return <ErrorHandler /> }
 		//else if (!this.props.robot.id) { return <LoadingScreen />}
 
@@ -46,6 +49,11 @@ class User extends React.Component {
 					<h2 className='user-name'>
 						Welcome, {thisUser.name}! What a purrfect day!
 					</h2>
+					<h3>{user.username}</h3>
+					<h3>{user.shippingAddress}</h3>
+					<h3>{user.billingAddress}</h3>
+					<h3>{user.prevOrders}</h3>
+					<h3>{user.favProducts}</h3>
 					<div>
 						<Link to={`/user/edit/${thisUser.id}`}>
 							<button type='button' className='edit-button' name={thisUser.id}>
@@ -64,7 +72,9 @@ const mapState = (state) => {
 };
 const mapDispatch = (dispatch) => {
 	return {
-		getUser: (userId, username) => dispatch(fetchUser(userId, username)),
+		getSingleUser: (userId, username) =>
+			dispatch(fetchSingleUser(userId, username)),
+
 		//,
 		//edit thunk?
 	};
