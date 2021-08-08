c52b0108f84568c275e593eff768d16190ae014b
Taissiya Ugay <taissiya.ugay@gmail.com>
2021-08-08 15:18:30 -0500

    added the total quantity of products user has in the cart

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -29,60 +29,66 @@ import { Link } from 'react-router-dom';
 // 	</div>
 // );
 
-class Navbar extends React.Component{
+class Navbar extends React.Component {
+	render() {
+		const { handleClick, isLoggedIn, currentUser } = this.props;
 
-  render(){
-    const { handleClick, isLoggedIn, currentUser } = this.props;
+		let allUsersButton = null;
 
-    let allUsersButton = null;
-
-    if (currentUser.isAdmin){
+		if (currentUser.isAdmin) {
 			allUsersButton = (
 				<button type="submit">
 					<Link to={`/users`}>All Users</Link>
 				</button>
 			);
 		}
-
-    return(
-      <div>
-    <h1>Welcome to Catsco!</h1>
-    <nav>
-      {isLoggedIn ? (
-        <div>
-          {/* The navbar will show these links after you log in */}
-          <Link to="/home">Home</Link>
-          <a href="#" onClick={handleClick}>
-            Logout
-          </a>
-          {allUsersButton}
-        </div>
-      ) : (
-        <div>
-          {/* The navbar will show these links before you log in */}
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-        </div>
-      )}
-      <Link to="/products">Products</Link>
-      <Link to="/cart">
-				<img src="/pics/cart.png" id="catCart"></img>
-			</Link>
-    </nav>
-    <hr />
-  </div>
-    );
-  }
+		const howManyItemsInTheCart = JSON.parse(
+			localStorage.getItem('cart')
+		).reduce((total, product) => {
+			total += product.quantity;
+			return total;
+		}, 0);
+		console.log(howManyItemsInTheCart);
+		return (
+			<div>
+				<h1>Welcome to Catsco!</h1>
+        {howManyItemsInTheCart && <h2>You have {howManyItemsInTheCart} items in your cart</h2>}
+				<nav>
+					{isLoggedIn ? (
+						<div>
+							{/* The navbar will show these links after you log in */}
+							<Link to="/home">Home</Link>
+							<a href="#" onClick={handleClick}>
+								Logout
+							</a>
+							{allUsersButton}
+						</div>
+					) : (
+						<div>
+							{/* The navbar will show these links before you log in */}
+							<Link to="/login">Login</Link>
+							<Link to="/signup">Sign Up</Link>
+						</div>
+					)}
+					<Link to="/products">Products</Link>
+					<Link to="/cart">
+						<img src="/pics/cart.png" id="catCart"></img>
+					</Link>
+				</nav>
+				<hr />
+			</div>
+		);
+	}
 }
 
 /**
  * CONTAINER
  */
-const mapState = state => {
-  return {
-    isLoggedIn: !!state.auth.id,
-    currentUser: state.auth
-  }
-}
+const mapState = (state) => {
+	return {
+		isLoggedIn: !!state.auth.id,
+		currentUser: state.auth,
+	};
+};
 
 export default connect(mapState)(Navbar);
